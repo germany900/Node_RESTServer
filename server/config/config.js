9df5ef50892a604d49752e691bf6f8aaf60c0a41
German Manuel Juarez Morales <ninganegro@hotmail.com>
2020-08-21 02:37:33 -0500

    Variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -16,6 +16,6 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://germany900:3l4rt3d3v1v1r@cafe-user.hf3mj.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
